b542627a3a74ccf7286b3c2c5df583237a67b4f4
Thomas Wedler <tix@gmx.li>
2024-07-07 21:16:37 +0800

    Add files via upload

--- api/api_basiccall.py
+++ api/api_basiccall.py
@@ -8,14 +8,32 @@ import pandas as pd
 import os.path
 import sys
 from flask import Flask, jsonify, request
+from flask_restplus import Api, Resource, fields
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Time Series API',
+          description='A simple Time Series API')
+
+ns = api.namespace('data', description='Time series operations')
 
 # Sample time series data
 modpath = os.path.dirname(os.path.abspath(sys.argv[0])) 
 datapath = os.path.join(modpath, '../../dataportal/tokens/SNEK.csv') 
 time_series_data = pd.read_csv(datapath)
 
+@ns.route('/')
+class DataResource(Resource):
+    @api.doc(params={'date': 'The date for the data'})
+    @api.response(200, 'Success')
+    @api.response(404, 'Date not found')
+    def get(self):
+        """Fetch the data for a given date"""
+        date = request.args.get('date')
+        if date in time_series_data:
+            return jsonify({date: time_series_data[date]})
+        else:
+            return jsonify({"error": "Date not found"}), 404
+        
 @app.route('/data', methods=['GET'])
 def get_data():
     date = request.args.get('date')
